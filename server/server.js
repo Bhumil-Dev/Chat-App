d8ee0f2f0b54f730872083ac076b988548982851
Bhumil Prajapati <bhumilprajapati4@gmail.com>
2025-07-15 18:38:14 +0530

    fix: enable server listen in production

--- server/server.js
+++ server/server.js
@@ -1,49 +1,51 @@
 import express from 'express'
 import 'dotenv/config'
-import cors from'cors'
+import cors from 'cors'
 import http from 'http'
 import { connectDB } from './lib/db.js'
 import userRouter from './routes/userRoutes.js'
 import messageRouter from './routes/messageRoutes.js'
 import { Server } from 'socket.io'
-import { deflate } from 'zlib'
 
 const app = express()
 const server = http.createServer(app)
 
-export const io = new Server(server,{
-    cors : {origin: "*"}
+// Socket.io setup
+export const io = new Server(server, {
+  cors: { origin: '*' }
 })
 
-export const userSocketMap = {};
+export const userSocketMap = {}
 
-io.on("connection",(socket)=>{
-    const userId = socket.handshake.query.userId;
-    console.log("User Connected",userId);
+io.on('connection', (socket) => {
+  const userId = socket.handshake.query.userId
+  console.log('User Connected', userId)
 
-    if(userId) userSocketMap[userId] = socket.id;
+  if (userId) userSocketMap[userId] = socket.id
 
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected", userId);
-        delete userSocketMap[userId]
-        io.emit("getOnlineUsers",Object.keys(userSocketMap))
-    })
+  io.emit('getOnlineUsers', Object.keys(userSocketMap))
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected', userId)
+    delete userSocketMap[userId]
+    io.emit('getOnlineUsers', Object.keys(userSocketMap))
+  })
 })
 
-app.use(express.json({limit : "4mb"}))
+// Middlewares
+app.use(express.json({ limit: '4mb' }))
 app.use(cors())
 
-app.use("/api/status",(req,res)=>res.send("server is live"))
-app.use("/api/auth",userRouter)
-app.use("/api/messages",messageRouter)
+// Routes
+app.use('/api/status', (req, res) => res.send('server is live'))
+app.use('/api/auth', userRouter)
+app.use('/api/messages', messageRouter)
 
-await connectDB();
+// Connect DB and start server
+await connectDB()
 
-if(process.env.NODE_ENV !== "production"){
-    const PORT = process.env.PORT || 5000
-    server.listen(PORT, ()=> console.log("Server is running on PORT:"+ PORT));
-}
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => console.log(`✅ Server is running on PORT: ${PORT}`))
 
-export default server;
-//export server for vercel
+// Export for vercel (if needed)
+export default server
